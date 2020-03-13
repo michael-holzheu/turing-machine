e386bea108a669e357a1cb1afab3ca6b74237d75
Michael Holzheu <michael@holzheu.de>
2020-03-14 00:58:59 +0100

    machine: Split main from run
    Signed-off-by: Michael Holzheu <michael@holzheu.de>

--- machine.c
+++ machine.c
@@ -8,13 +8,15 @@
 
 #define BLANK		'.'
 #define TAPE_START	8
+#define FINAL		-1
+
 uint8_t tape[16];
 int tape_pos = TAPE_START;
 int state_curr = 0;
 
 /* Turing instruction */
 
-enum move {LEFT = 'L', RIGHT = 'R', HALT = 'H'};
+enum move {LEFT = 'L', RIGHT = 'R', NO_SHIFT = 'N'};
 
 struct instr {
 	int state_curr;		/* Current state of machine */
@@ -23,22 +25,6 @@ struct instr {
 	enum move move;		/* Move move */
 	int state_new;		/* New state for machine */
 };
-
-/* Action table */
-
-struct instr instr_program[] = {
-	/* State 0: Search rightmost digit */
-	{0, '0',     '0',     RIGHT, 0}, /* Skip 0s */
-	{0, '1',     '1',     RIGHT, 0}, /* Skip 1s */
-	{0, BLANK,    BLANK,  LEFT,  1}, /* Found blank */
-	/* State 1: Do addition with 1 */
-	{1, '0',     '1',     LEFT,  2}, /* 0 + 1 = 1 > done */
-	{1, '1',     '0',     LEFT,  1}, /* 1 + 1 = 0 + carry */
-	/* State 2: Accepting State */
-	{2, '*',     '*',     HALT,  2},
-};
-
-uint8_t tape_input[] = {'1', '0', '1', '1'};
 
 void state_print(struct instr *instr)
 {
@@ -72,8 +58,8 @@ void state_print(struct instr *instr)
 	printf("\n");
 }
 
-
-int main(void)
+int machine_run(struct instr* instr_vec, int instr_count,
+		uint8_t *input, int input_count)
 {
 	struct instr *instr;
 	int i;
@@ -81,15 +67,26 @@ int main(void)
 	/* Initialize tape with blanks */
 	memset(tape, BLANK, sizeof(tape));
 	/* Add input */
-	memcpy(&tape[TAPE_START], tape_input, sizeof(tape_input));
+	memcpy(&tape[TAPE_START], input, input_count);
 
 	while (1) {
-		for (i = 0; i < ARRAY_LEN(instr_program); i++) {
-			instr = &instr_program[i];
+		for (i = 0; i < instr_count; i++) {
+			instr = &instr_vec[i];
 			if (instr->state_curr != state_curr)
-				continue;
-			if (instr->read != tape[tape_pos] && instr->read != '*')
-				continue;
+				goto no_match;
+			if (instr->read == tape[tape_pos])
+				goto match;
+			if (instr->read == '*')
+				goto match;
+			if (instr->read == '+' && tape[tape_pos] != BLANK)
+				goto match;
+no_match:
+			continue;
+match:
+			if (instr->state_new == FINAL) {
+				printf("END: State: %d\n", instr->state_curr);
+				exit(0);
+			}
 			state_print(instr);
 			/* Write new data */
 			if (instr->write != '*')
@@ -104,9 +101,8 @@ int main(void)
 				case RIGHT:
 					tape_pos++;
 					break;
-				case HALT:
-					printf("END: State: %d\n", state_curr);
-					exit(0);
+				case NO_SHIFT:
+					break;
 			}
 			goto found;
 		}
@@ -116,4 +112,26 @@ found:
 		continue;
 	}
 	return 0;
+}
+
+/* Action table */
+
+struct instr instr_program[] = {
+	/* State 0: Search rightmost digit */
+	{0, '0',     '0',     RIGHT, 0}, /* Skip 0s */
+	{0, '1',     '1',     RIGHT, 0}, /* Skip 1s */
+	{0, BLANK,    BLANK,  LEFT,  1}, /* Found blank */
+	/* State 1: Do addition with 1 */
+	{1, '0',     '1',     LEFT,  2}, /* 0 + 1 = 1 > done */
+	{1, '1',     '0',     LEFT,  1}, /* 1 + 1 = 0 + carry */
+	/* State 2: Accepting State */
+	{2, '*',     '*',     NO_SHIFT, FINAL},
+};
+
+uint8_t tape_input[] = {'1', '0', '1', '1'};
+
+int main(void)
+{
+	return machine_run(instr_program, ARRAY_LEN(instr_program),
+			tape_input, sizeof(tape_input));
 }
