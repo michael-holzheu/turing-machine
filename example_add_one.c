3fe5a083dac7971d3232aaa5e4021c5dacc15dd9
Michael Holzheu <michael@holzheu.de>
2020-03-15 16:00:44 +0100

    Add missing file
    Signed-off-by: Michael Holzheu <michael@holzheu.de>

---
+++ example_add_one.c
@@ -0,0 +1,28 @@
+/*
+ * Add one to binary number
+ */
+
+#include "helper.h"
+#include "machine.h"
+
+/* Action table */
+
+struct instr instr_program[] = {
+	/* State a: Search rightmost digit */
+	{'a', '0',     '0',     RIGHT, 'a'}, /* Skip 0s */
+	{'a', '1',     '1',     RIGHT, 'a'}, /* Skip 1s */
+	{'a', BLANK,    BLANK,  LEFT,  'b'}, /* Found blank */
+	/* State b: Do addition with 1 */
+	{'b', '0',     '1',     LEFT,  'c'}, /* 0 + 1 = 1 > done */
+	{'b', '1',     '0',     LEFT,  'b'}, /* 1 + 1 = 0 + carry */
+	/* State 2: Accepting State */
+	{'c', '*',     '*',     NO_SHIFT, FINAL},
+};
+
+uint8_t tape_input[] = {'1', '0', '1', '1'};
+
+int main(void)
+{
+	return machine_run(instr_program, ARRAY_LEN(instr_program),
+			tape_input, sizeof(tape_input), 'a');
+}
